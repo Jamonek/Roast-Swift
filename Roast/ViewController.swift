101f888c9abb251a0b7d1cd1aca3d39a391debb3
Jamone Kelly <xg.monie089@gmail.com>
2015-04-14 15:33:34 -0400

    Initial Commit

---
+++ Roast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roast
+//
+//  Created by Jamone Alexander Kelly on 4/14/15.
+//  Copyright (c) 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
